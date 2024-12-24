7ddaaa78c557dac11d627bf34f562810cc784a6e
Stefan Saveski <76560180+stefansaveski@users.noreply.github.com>
2024-12-24 20:18:13 +0100

    Create two-sum.java

---
+++ Arrays & Hashing/two-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int dif = target - nums[i];
+            if(map.containsKey(dif)){
+                return new int[] {map.get(dif), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
