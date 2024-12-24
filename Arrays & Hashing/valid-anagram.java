77a8eccc67151352d8eaaa31edfabd79a0377025
Stefan Saveski <76560180+stefansaveski@users.noreply.github.com>
2024-12-24 20:00:36 +0100

    Create valid-anagram.java

---
+++ Arrays & Hashing/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabetA = new int[128];
+        int[] alphabetB = new int[128];
+        if(s.length() != t.length())
+            return false;
+        for(int i=0; i<s.length(); i++){
+            alphabetA[s.charAt(i)] += 1;
+            alphabetB[t.charAt(i)] += 1;
+        }
+        for(int i=0; i<128; i++){
+            if (alphabetA[i] != alphabetB[i])
+                return false;
+        }
+        return true;
+    }
+}
