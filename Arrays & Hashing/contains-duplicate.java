2e0294cbcc029d2362cd4e202419023c63097c81
Stefan Saveski <76560180+stefansaveski@users.noreply.github.com>
2024-12-24 19:39:42 +0100

    Create contains-duplicate.java

---
+++ Arrays & Hashing/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            boolean seen = set.contains(nums[i]);
+            if (seen) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
