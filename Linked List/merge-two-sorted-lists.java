51fd0d0956af97082a9dfc850130c3f625cc80aa
Stefan Saveski <76560180+stefansaveski@users.noreply.github.com>
2025-01-02 19:32:24 +0100

    Create merge-two-sorted-lists.java

---
+++ Linked List/merge-two-sorted-lists.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode result;
+        if (list1.val < list2.val) {
+            result = list1;
+            list1 = list1.next;
+        } else {
+            result = list2;
+            list2 = list2.next;
+        }
+
+        ListNode curr = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else {
+            curr.next = list2;
+        }
+
+        return result;
+    }
+}
